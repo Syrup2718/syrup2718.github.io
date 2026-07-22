1a452008ef74d1f741692a8f992e645fec588ee9
syrup2718 <ann200611@gmail.com>
2026-07-22 13:45:58 +0800

    build Astro blog

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = '糖漿の小窩';
+export const SITE_DESCRIPTION = '我的小小祕密基地';
